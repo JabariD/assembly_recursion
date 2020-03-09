59f1ef28a966e94f594fd21f3b5d666af7a6903e
Payton Dennis <52969350+JabariD@users.noreply.github.com>
2020-03-09 18:49:52 -0400

    .data
    I will try to limit comments to 5 lines and explain as much as possible. I will admit that the problems that the bugs that came up with I just developed a solution to solve that bug and tried to fit it in as best as possible.
    
    So, with that being said, code may seem all over the place, but I'll do my best to explain through commits and comments on what I was thinking through this process

--- recursion.s
+++ recursion.s
@@ -1,1 +1,18 @@
+.data
 
+errorMSG: .asciiz "Invalid input"
+userInput: .space 101
+new_userInput: .space 101
+
+baseNumber: .space 3
+currNumber: .space 3
+
+# Store baseNumber * currNumber into tempAnswer. # do it counte rnumber of times
+tempAnswer: .space 100       
+# Temporary Storage
+tempProduct: .space 100
+
+# Add tempFinalAnswer to FinalAnswer
+finalAnswer: .space 100
+
+.text
