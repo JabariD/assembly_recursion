f8be9b28928c92028373c27a81eba424341366d5
Payton Dennis <52969350+JabariD@users.noreply.github.com>
2020-03-09 21:43:42 -0400

    Store baseNumber
    Here we simply store my baseNumber. 3 (51) goes into pos0. And 51 goes into pos1.
    
    It is important to note that THE STRING CHARACTERS are REVERSED! Meaning if we were going to represent C in a string. We would represent it as 2 in pos0 and 1 in pos1. Giving us 21. Reversed is 12.

--- recursion.s
+++ recursion.s
@@ -16,3 +16,14 @@ tempProduct: .space 100
 finalAnswer: .space 100
 
 .text
+
+# NOTE: characters are reversed!
+
+main:
+#init base
+addi $gp, $zero, 0      # init -- carry for Final Addition
+la $t0, baseNumber      # load address of baseNumber
+li $t1, 51              # get 3 in ascii
+sb $t1, 0($t0)          # store 3 in ascii
+addi $t0, $t0, 1        # go to next ch spot
+sb $t1, 0($t0)          # store 3 in ascii
